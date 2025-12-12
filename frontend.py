3a7c1ec547de8ff1060943eef51d68224f13726c
Mohammad-Hassan027 <shaikhmohammadhassan027@gmail.com>
2025-12-12 16:35:36 +0530

    add backend deployed url and requirements.txt for forntend in root

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # --- CONFIGURATION ---
-BACKEND_URL = "http://127.0.0.1:8000"  # Ensure this matches your FastAPI address
+BACKEND_URL = "https://cyber-guardian-backend.onrender.com"  # Ensure this matches your FastAPI address http://127.0.0.1:8000
 
 st.set_page_config(
     page_title="CyberGuardian APK Scanner",
